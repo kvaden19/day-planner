7bf9ee89d6e90133f536f68a4265c1ff4ad9fec2
Kelly Vaden <klvaden@gmail.com>
2021-03-08 17:36:45 -0500

    Added timeblocks from 8AM to 6PM to the page using Moment.js (no styling yet)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 // Define DOM variables
 var currentDayP = $("#currentDay");
+var timeBlockContainer = $(".container");
 
 // Define other stuff
-var dayOfWeek = moment().format('dddd');
+var dayOfWeek = moment().format('dddd'); // home page
 var date = moment().format('MMMM Do');
 
+// Display stuff
 currentDayP.text("Today is " + dayOfWeek + ", " + date);
+
+for (i=8; i<19; i++) {
+    var hour = moment({ hour:i, minute:0 }).format('h:mm A'); // Docs - Parse - Object
+    var htmlContent = "<p>" + hour + "</p>";
+    timeBlockContainer.append(htmlContent);
+}
+
+
+
 
